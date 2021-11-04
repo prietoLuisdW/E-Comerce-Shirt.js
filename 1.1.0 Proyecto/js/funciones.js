3d662c93d912a9fd7f5a61983ad954cccd95d565
prietoLuisdW <lhprieto91@gmail.com>
2021-11-04 10:35:18 -0500

    Estructura con Vanilla.js

---
+++ 1.1.0 Proyecto/js/funciones.js
@@ -0,0 +1,39 @@
+function renderizarProductos() {
+    let articulos = document.createElement("div")
+    articulos.setAttribute("class", "row mx-1 d-flex justify-content-around")
+
+    for (const hoddie of hoddies) {
+        // contenedor
+        let articulo = document.createElement("article")
+        articulo.setAttribute("class", "card mx-1 col-lg-3 col-md-6 col-sm-12 my-3 pt-2 productos__articulo")
+            //Imagen
+        let imagen = document.createElement("img")
+        imagen.setAttribute("class", "card-img-top")
+        imagen.src = hoddie.imagen
+        articulo.appendChild(imagen)
+            //Informacion del articulo
+        let info = document.createElement("div")
+        info.setAttribute("class", "card-body")
+        articulo.appendChild(info)
+            //Nombre del producto
+        let nombre = document.createElement("h5")
+        nombre.setAttribute("class", "card-title text-center")
+        nombre.textContent = hoddie.nombre
+        info.appendChild(nombre)
+            //Precio del Producto
+        let precio = document.createElement("h5")
+        precio.textContent = "$" + hoddie.precio
+        precio.setAttribute("class", "card-title text-center")
+        info.appendChild(precio)
+            //Boton para agregar al carrito
+        let botonCarrito = document.createElement("button")
+        botonCarrito.classList.add("btn", "btn-outline-dark", "w-100", "btn__carrito")
+        botonCarrito.textContent = "Agregar al Carrito"
+        botonCarrito.setAttribute("marcador", hoddie.ref)
+        botonCarrito.setAttribute("id", hoddie.ref)
+            // botonCarrito.addEventListener("click", agregarCarrito("${hoddie.ref}"))
+        info.appendChild(botonCarrito)
+        articulos.appendChild(articulo)
+    }
+    document.getElementById("mostrador").appendChild(articulos)
+}
